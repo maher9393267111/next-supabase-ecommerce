50c48d9d06d11bc7e947268bac3635cdb3ffa2a6
maher <gomemahero.com>
2022-08-11 13:27:20 +0300

    react-lottie animation start work good

---
+++ components/animate.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import animationData from "../helper/success-animation.json"
+import Lottie from "react-lottie"
+const Animate = () => {
+    return (
+        <div>
+              <div>
+          <Lottie
+            options={{ animationData: animationData, loop: false }}
+            height="50px"
+            width="50px"
+          ></Lottie>
+        </div>
+        </div>
+    );
+}
+
+export default Animate;
